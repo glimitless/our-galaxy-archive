5cfdb0ceca1da874e147f87f2003ed0195460ab6
glimitless <158614739+glimitless@users.noreply.github.com>
2024-03-19 20:21:52 -0400

    back to normal

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 
-import * as THREE from 'node_modules/three';
-import { OrbitControls } from 'node_modules/three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 // let frameCount = 0;
 // const throttleInterval = 0; // Adjust the interval as needed
